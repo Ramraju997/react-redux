b23986d85dee41ff30ecd2279e36dce001406146
Ram Raju <84369007+Ramraju997@users.noreply.github.com>
2023-08-02 08:30:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/counter";
+export default function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1>The Count is {count}</h1>
+      <button onClick={() => dispatch(increment)}>Increment</button>
+      <button onClick={() => dispatch(decrement)}>Decrement</button>
+    </div>
+  );
+}
